ca24c83c1dac47da76b1d0d6c20faeb12d335af6
janainfanger <43797937+janainfanger@users.noreply.github.com>
2019-01-16 12:04:48 -0800

    Create model classes for the python club database.
    created tables in our database using python, and also created a superuser for admin (this is what you need for assignment 4 in pythonclub

--- techreview/review/admin.py
+++ techreview/review/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import ProductType, Product, Review
 
 # Register your models here.
+
+admin.site.register(ProductType)
+admin.site.register(Product)
+admin.site.register(Review)
+
+#create
+
+
--- techreview/review/views.py
+++ techreview/review/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'review/index.html')#tells it to render a webpage
+
