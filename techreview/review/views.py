3f9d6eebf66b1ab7e3a822d180742b9e24fe428e
janainfanger <43797937+janainfanger@users.noreply.github.com>
2019-01-28 11:47:32 -0800

    Adds a view page
    To list items in our database

--- techreview/review/views.py
+++ techreview/review/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from.models import ProductType
 
 # Create your views here.
 def index(request):
     return render(request, 'review/index.html')#tells it to render a webpage
 
+def techtypes (request):
+    type_list=ProductType.objects.all()
+    return render(request, 'review/types.html', {'type list': type_list})
